604d370b8dce82ec313ff257c630c73a75544d48
Murtaza Haidary <LE1983326@crc-lennox.qc.ca>
2024-11-11 18:05:41 -0500

    Prison template 3 finished

--- Script/prison_guard.gd
+++ Script/prison_guard.gd
@@ -11,3 +11,9 @@ extends CharacterBody3D
 @onready var prison_guard = $"."
  
 @export var patrol_markers: Array[Marker3D] = []
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body == prison_guard:
+		state_machine.transition_to("Attack")
+		
